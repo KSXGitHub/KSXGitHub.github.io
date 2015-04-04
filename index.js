a6065855823ae526b6dbe0165c6fc0e85c993a5a
KSXGitHub <hvksmr1996@gmail.com>
2015-04-04 19:06:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+function main(){
+	var htmlns = "http://www.w3.org/1999/xhtml";
+	var bodyElement = document.body;
+	var tipText = document.createTextNode("Hello! Click the button below to view the message");
+	var buttonText = document.createTextNode("Click here");
+	var tipElement = document.createElementNS(htmlns, "div");
+	var button = document.createElementNS(htmlns, "button");
+	var buttonContainer = document.createElementNS(htmlns, "div");
+	bodyElement.insertBefore(tipElement, null);
+	bodyElement.insertBefore(buttonContainer, null);
+	tipElement.insertBefore(tipText, null);
+	buttonContainer.insertBefore(button, null);
+	button.insertBefore(tipText, null);
+	return 0;
+}
+
+main();
