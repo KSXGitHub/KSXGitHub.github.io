47ae94b0feab1d6e82ee2f7678c50c7e7577bb5b
KSXGitHub <hvksmr1996@gmail.com>
2015-04-04 19:14:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 
 function main(){
-	var htmlns = "http://www.w3.org/1999/xhtml";
-	var bodyElement = document.body;
-	var tipText = document.createTextNode("Hello! Click the button below to view the message");
-	var buttonText = document.createTextNode("Click here");
-	var tipElement = document.createElementNS(htmlns, "div");
-	var button = document.createElementNS(htmlns, "button");
-	var buttonContainer = document.createElementNS(htmlns, "div");
-	bodyElement.insertBefore(tipElement, null);
-	bodyElement.insertBefore(buttonContainer, null);
-	tipElement.insertBefore(tipText, null);
-	buttonContainer.insertBefore(button, null);
-	button.insertBefore(buttonText, null);
+	function elements_creation(){
+		var htmlns = "http://www.w3.org/1999/xhtml";
+		var bodyElement = document.body;
+		var tipText = document.createTextNode("Hello! Click the button below to view the message");
+		var buttonText = document.createTextNode("Click here");
+		var tipElement = document.createElementNS(htmlns, "div");
+		var button = document.createElementNS(htmlns, "button");
+		var buttonContainer = document.createElementNS(htmlns, "div");
+		bodyElement.insertBefore(tipElement, null);
+		bodyElement.insertBefore(buttonContainer, null);
+		tipElement.insertBefore(tipText, null);
+		buttonContainer.insertBefore(button, null);
+		button.insertBefore(buttonText, null);
+		return 0;
+	}
+	function events_creation(){
+		button.addEventListener("click", button_onClick, false);
+		return 0;
+	}
+	elements_creation();
+	events_creation();
 	return 0;
 }
 
