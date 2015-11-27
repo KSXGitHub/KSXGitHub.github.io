58d92601bed6a05c6aa6b5cf2dcfabc72ff19059
Khải <hvksmr1996@gmail.com>
2015-11-27 08:33:11 +0700

    Update main.js

--- myexamples/prime-generator/main.js
+++ myexamples/prime-generator/main.js
@@ -1,1 +1,38 @@
 
+(function (document) {
+  'use strict';
+  
+  var input = document.getElementById('input');
+  var output = document.getElementById('output');
+  
+  input.addEventListener('change', onchange, false);
+  
+  function onchange() {
+    
+    var n = parseInt(input.value);
+    
+    if (!isFinite(n)) {
+      output.textContent = "Invalid input";
+      return;
+    }
+    
+    if (n < 2) {
+      output.textContent = "No prime less than 2";
+      return;
+    }
+    
+    var out = [2];
+    
+    for (let prime of new PrimeGenerator()) {
+      if (!n) {
+        break;
+      }
+      out.push(prime);
+      --n;
+    }
+    
+    output.textContent = out.join(', ');
+    
+  }
+  
+})(document);
