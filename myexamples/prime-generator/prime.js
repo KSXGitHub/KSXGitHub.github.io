21f814f69286e661b74988b9f4ebda6d6b3f00bb
Khải <hvksmr1996@gmail.com>
2015-11-27 08:19:14 +0700

    Update prime.js

--- myexamples/prime-generator/prime.js
+++ myexamples/prime-generator/prime.js
@@ -1,1 +1,56 @@
 
+function main(out) {
+  'use strict';
+  
+  class PrimeGenerator {
+    
+    constructor(List, loop) {
+      
+			if (typeof List !== "function") {
+				List = Set;
+			}
+			
+			if (typeof loop !== "function") {
+				loop = () => 1;
+			}
+			
+			var primegenerator = this;
+			
+			primegenerator[Symbol.iterator] = primegenerator.entries = PrimeIterator;
+			
+			function * PrimeIterator() {
+			
+				var allprimes = new List();
+				var current = 3;
+					
+				// Loop: "Find Prime"
+				for ( ; loop(current); current += 2) {
+					
+					let isprime = 1;
+					
+					// Loop: "Find Composite"
+					for (let divisor of allprimes) {
+						if (!(current % divisor)) {
+							isprime = 0;
+							break; // Escape "Find Composite", go to the rest of "Find Prime"
+						}
+					}
+					
+					if (isprime) {
+						yield current;
+						allprimes.add(current);
+					}
+				
+				}
+			
+			}
+			
+    }
+    
+  }
+  
+  out.PrimeGenerator = PrimeGenerator;
+  
+}
+
+main(window);
