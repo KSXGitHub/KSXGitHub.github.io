56bf5be2d20ba2dac1034297668dd6b91dc07fd4
Khải <hvksmr1996@gmail.com>
2016-10-14 21:55:54 +0700

    Update script.js

--- myexamples/fractals/triangle/script.js
+++ myexamples/fractals/triangle/script.js
@@ -2,19 +2,32 @@
 
 main(window)
 
-function main ({Math: {sqrt, sin, cos}, document}) {
+function main ({Math: {sqrt, sin, cos, PI}, document}) {
   const triangleheight = sqrt(3) / 2
-  const rotate = ([x, y], alpha) => {
+  const rotate = alpha => (x, y) => {
     const sa = sin(alpha)
     const ca = cos(alpha)
     return [x * ca - y * sa, x * sa + y * ca]
   }
   function * fractal (alpha, count) {
     if (count) {
-      const next = fractal(alpha, count - 1)
-      yield * [...next].map(([x, y]) => [x / 3, y / 3])
-      yield ''
+      const rotation = rotate(alpha)
+      const next = [...fractal(alpha, count - 1)]
+      yield * next
+        .map(([x, y]) => [x / 3, y / 3])
+      yield rotation(1 / 3, 0)
+      yield * [...fractal(alpha + PI * 2 / 3, count - 1)]
+        .map(([x, y]) => [x / 3 + 1 / 3, y / 3])
+      yield rotation(1 / 2, 0)
+      yield * [...fractal(alpha - PI * 2 / 3, count - 1)]
+        .map(([x, y]) => [x / 3 + 1 / 2, y / 3])
+      yield rotation(2 / 3, 0)
+      yield * next
+        .map(([x, y]) => [x / 3 + 2 / 3, y / 3])
     }
+  }
+  function * koch (count) {
+    
   }
   const canvas = document.getElementById('canvas')
   const context = canvas.getContext('2d')
