237b37553d325038820be1af25564783f03b95c3
Khải <hvksmr1996@gmail.com>
2016-10-14 21:34:43 +0700

    Update script.js

--- myexamples/fractals/triangle/script.js
+++ myexamples/fractals/triangle/script.js
@@ -2,6 +2,20 @@
 
 main(window)
 
-function main ({Math: {sqrt}, document}) {
-  function * fractal (begin, end, count = 0) {}
+function main ({Math: {sqrt, sin, cos}, document}) {
+  const triangleheight = sqrt(3) / 2
+  const rotate = ([x, y], alpha) => {
+    const sa = sin(alpha)
+    const ca = cos(alpha)
+    return [x * ca - y * sa, x * sa + y * ca]
+  }
+  function * fractal (alpha, count) {
+    if (count) {
+      const next = fractal(alpha, count - 1)
+      yield * [...next].map(([x, y]) => [x / 3, y / 3])
+      yield ''
+    }
+  }
+  const canvas = document.getElementById('canvas')
+  const context = canvas.getContext('2d')
 }
