16fa3ee1b04582a8d6174073fd84e2ac03845f66
Khải <hvksmr1996@gmail.com>
2016-10-23 23:45:47 +0700

    Update script.js

--- myexamples/random-decay/script.js
+++ myexamples/random-decay/script.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const {random} = Math
+const {random, floor} = Math
 const DONOTHING = () => {}
+const xmlns = document.documentElement.namespaceURI
 
 const range = (begin, end) =>
   begin < end ? [begin, ...range(begin + 1, end)] : []
@@ -25,6 +26,39 @@ const log = document.getElementById('log')
 const start = document.getElementById('start')
 const stop = document.getElementById('stop')
 
+let timerid = null
+
+const onStart = () => {
+  const paragraph = document.createElementNS(xmlns, 'p')
+  const headerdiv = document.createElementNS(xmlns, 'div')
+  log.appendChild(paragraph)
+  paragraph.appendChild(headerdiv)
+  headerdiv.innerHTML = `<h3><span>${new Date()}</span></h3>`
+  const main = ({timer, array}) => {
+    timerid = timer
+    const div = document.createElementNS(xmlns, 'div')
+    const length = document.createElementNS(xmlns, 'span')
+    const list = document.createElementNS(xmlns, 'span')
+    paragraph.appendChild(div)
+    div.appendChild(length)
+    div.appendChild(list)
+    length.textContent = array.length
+    content.textContent = array.join(', ')
+  }
+  decay(
+    range(0, floor(count.value)),
+    floor(delay.value),
+    parseFloat(chance.value),
+    main
+  )
+}
+
+const onStop = () => {
+  clearTimeout(timerid)
+  log.textContent = ''
+}
+
 turn(start, stop)
 turn(stop, start)
-
+onClick(start, onStart)
+onClick(stop, onStop)
